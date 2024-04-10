32d6e56b7f0ed8d0afdc3462f7847dda059c093e
RomainC75 <rom.chenard@gmail.com>
2024-04-10 15:26:17 +0200

    zerolog colors

--- server/main.go
+++ server/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "server/cmd"
+import (
+	"os"
+	"server/cmd"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	cmd.Execute()
 }
