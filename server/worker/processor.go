c0782c6e4c6cbf2d0c8606c341ea3d0a5118546f
RomainC75 <rom.chenard@gmail.com>
2024-04-10 15:13:43 +0200

    redis error handling

--- server/worker/processor.go
+++ server/worker/processor.go
@@ -5,6 +5,7 @@ import (
 	db "server/db/sqlc"
 
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -33,6 +34,10 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 				DefaultQueue:  3,
 				LowQueue:      1,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).
+					Bytes("payload", task.Payload()).Msg("process task failed")
+			}),
 		},
 	)
 
