55aff8b7cf68d865976a23a93f3833045d7e1213
Romain Chenard <romain.chenard@saegus.com>
2024-04-24 20:16:07 +0200

    loop ended

--- shared/events/api_processor.go
+++ shared/events/api_processor.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	work_dto "shared/dto"
+	"server/utils"
 	"shared/scenarios"
 
 	"github.com/hibiken/asynq"
@@ -12,40 +12,17 @@ import (
 )
 
 func (processor *RedisTaskProcessor) ProcessPortScannerResponse(ctx context.Context, task *asynq.Task) error {
-	var payload work_dto.PortTestScenario
+	fmt.Println("===========GET RESPONSE================")
+	var payload scenarios.ScanResult
 	if err := json.Unmarshal(task.Payload(), &payload); err != nil {
 		return fmt.Errorf("failed to unmarshal payload : %w", asynq.SkipRetry)
 	}
-	targetIp := payload.IPRange.IpMin
-	// user, err := processor.store.GetUserByEmail(ctx, payload.Username)
-
-	// TODO: send email to user
-
-	result, err := scenarios.Scan(targetIp, payload.PortRange.Min, payload.PortRange.Max)
-	fmt.Printf("===========FINISHED ================")
-	if err != nil {
-		log.Error().Str("scenario Error : ", err.Error())
-	}
-	// strResult, err := scenarios.GetString(result)
+	utils.PrettyDisplay("worker response", payload)
 
 	log.Info().Str("type", task.Type()).Bytes("payload", task.Payload()).
-		Str("targetIp", targetIp).Msg("PROCESSED task")
+		Str("targetIp", "lkj").Msg("PROCESSED task")
 	fmt.Println("==> RESPONSE worker => api")
 	//============================================
-	distributor := Get()
-	opts := []asynq.Option{
-		asynq.MaxRetry(10),
-		// asynq.ProcessIn(time.Second),
-		// + send to other queue :-)
-		asynq.Queue(string(CriticalQueueRes)),
-	}
-	fmt.Println("====> distributor : ", distributor)
-	err = distributor.DistributeTaskSendWorkBack(
-		ctx,
-		&result,
-		PortScanner,
-		opts...,
-	)
 
 	return nil
 }
