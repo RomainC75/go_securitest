c200f91047fe1d276cee5d0776a181868ae873a5
Romain Chenard <romain.chenard@saegus.com>
2024-04-24 22:25:12 +0200

    scan + port db

--- shared/db/migration/000001_init_schema.up.sql
+++ shared/db/migration/000001_init_schema.up.sql
@@ -6,6 +6,24 @@ CREATE TABLE users (
     updated_at TIMESTAMP NOT NULL
 );
 
+CREATE TABLE scans (
+    id   SERIAL PRIMARY KEY,
+    user_id   INT PRIMARY KEY,
+    date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE ports (
+    id SERIAL PRIMARY KEY,
+    scan_id INT NOT NULL, 
+    ip VARBINARY(16) NOT NULL, 
+    port INT NOT NULL,
+    state BOOLEAN,
+    FOREIGN KEY (scan_id) REFERENCES scans(id)
+);
+
+
 -- CREATE TABLE lists (
 --     id   SERIAL PRIMARY KEY,
 --     name TEXT NOT NULL,
