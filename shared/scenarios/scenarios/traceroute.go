7da5bc5268cb6cdd13290fbb10f83ecb42b292a2
Romain CHENARD <rom@Romains-MacBook-Air.local>
2024-12-08 21:33:49 +0100

    refactor: 💡 Traceroute - return dns name if found

--- shared/scenarios/scenarios/traceroute.go
+++ shared/scenarios/scenarios/traceroute.go
@@ -11,30 +11,38 @@ import (
 	"golang.org/x/net/ipv4"
 )
 
+type TargetInfos struct {
+	ip   string
+	name string
+}
+
 type STraceroute struct {
 	targetAddress string
-	routes        []string
+	routes        []TargetInfos
 }
 
 func NewTraceroute(targetAddress string) *STraceroute {
 	return &STraceroute{
 		targetAddress: targetAddress,
-		routes:        []string{},
+		routes:        []TargetInfos{},
 	}
 }
 
-func (fp *STraceroute) Run() ([]string, error) {
+func (fp *STraceroute) Run() ([]TargetInfos, error) {
 	ttl := 1
 	for {
 		addr, ok, pass, err := fp.send(ttl)
 		fmt.Println("--> addr : ", addr, ok, pass, ttl)
 		if err != nil && !ok && !pass {
-			return []string{}, err
+			return []TargetInfos{}, err
 		}
 		if !ok && !pass {
 			fp.routes = append(fp.routes, addr)
 		} else if !ok && pass {
-			fp.routes = append(fp.routes, "xxx")
+			fp.routes = append(fp.routes, TargetInfos{
+				ip:   "",
+				name: "",
+			})
 		} else {
 			return fp.routes, nil
 		}
@@ -43,26 +51,26 @@ func (fp *STraceroute) Run() ([]string, error) {
 
 }
 
-func (fp *STraceroute) send(ttl int) (string, bool, bool, error) {
+func (fp *STraceroute) send(ttl int) (TargetInfos, bool, bool, error) {
 	address := fp.targetAddress
 
 	conn, err := net.Dial("ip4:icmp", address)
 	if err != nil {
-		return "", false, false, fmt.Errorf("failed to dial: %v", err)
+		return TargetInfos{}, false, false, fmt.Errorf("failed to dial: %v", err)
 	}
 	defer conn.Close()
 
 	// Create ICMP connection
 	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
 	if err != nil {
-		return "", false, false, fmt.Errorf("error listening on ICMP: %v", err)
+		return TargetInfos{}, false, false, fmt.Errorf("error listening on ICMP: %v", err)
 	}
 	defer c.Close()
 
 	// Set TTL
 	ipConn := c.IPv4PacketConn()
 	if err := ipConn.SetTTL(ttl); err != nil {
-		return "", false, false, fmt.Errorf("failed to set TTL: %v", err)
+		return TargetInfos{}, false, false, fmt.Errorf("failed to set TTL: %v", err)
 	}
 
 	// Prepare ICMP message
@@ -79,38 +87,38 @@ func (fp *STraceroute) send(ttl int) (string, bool, bool, error) {
 	// Marshal the message
 	wb, err := wm.Marshal(nil)
 	if err != nil {
-		return "", false, false, fmt.Errorf("failed to marshal message: %v", err)
+		return TargetInfos{}, false, false, fmt.Errorf("failed to marshal message: %v", err)
 	}
 
 	// Send the packet
 	dst, err := net.ResolveIPAddr("ip4", address)
 	if err != nil {
-		return "", false, false, fmt.Errorf("failed to resolve address: %v", err)
+		return TargetInfos{}, false, false, fmt.Errorf("failed to resolve address: %v", err)
 	}
 
 	// Write to the destination
 	if _, err := c.WriteTo(wb, dst); err != nil {
-		return "", false, false, fmt.Errorf("failed to write to destination: %v", err)
+		return TargetInfos{}, false, false, fmt.Errorf("failed to write to destination: %v", err)
 	}
 
 	// Read response
 	rb := make([]byte, 1500)
 	err = c.SetReadDeadline(time.Now().Add(5 * time.Second))
 	if err != nil {
-		return "", false, false, fmt.Errorf("failed to set read deadline: %v", err)
+		return TargetInfos{}, false, false, fmt.Errorf("failed to set read deadline: %v", err)
 	}
 
 	n, peer, err := c.ReadFrom(rb)
 	if err != nil {
 		log.Printf("failed to read response: %v", err)
-		return "", false, true, fmt.Errorf("failed to read response: %v", err)
+		return TargetInfos{}, false, true, fmt.Errorf("failed to read response: %v", err)
 	}
 
 	// Parse the response
 	rm, err := icmp.ParseMessage(ipv4.ICMPTypeEchoReply.Protocol(), rb[:n])
 	if err != nil {
 		log.Printf("failed to parse response: %v", err)
-		return "", false, false, fmt.Errorf("failed to parse response: %v", err)
+		return TargetInfos{}, false, false, fmt.Errorf("failed to parse response: %v", err)
 	}
 
 	log.Println("TTYPE : ", rm.Type, rm.Code, peer)
@@ -118,11 +126,35 @@ func (fp *STraceroute) send(ttl int) (string, bool, bool, error) {
 	switch rm.Type {
 	case ipv4.ICMPTypeEchoReply:
 		log.Printf("got PONG from %v", peer)
-		return peer.String(), true, false, nil
+		addr := peer.String()
+		dnsName, err := net.LookupAddr(addr)
+		if err != nil {
+			log.Printf("cannot get address : %s\n", err.Error())
+			dnsName = append(dnsName, "")
+		} else {
+			log.Printf("+++ DNS : %s\n", dnsName)
+		}
+
+		return TargetInfos{
+			ip:   peer.String(),
+			name: dnsName[0],
+		}, true, false, nil
 	case ipv4.ICMPTypeTimeExceeded:
 		log.Printf("TTL Exceeded - Intermediate hop found at %v", peer)
-		return peer.String(), false, false, nil
+		addr := peer.String()
+		dnsName, err := net.LookupAddr(addr)
+
+		if err != nil {
+			log.Printf("cannot get address : %s\n", err.Error())
+			dnsName = append(dnsName, "")
+		} else {
+			log.Printf("+++ DNS : %s\n", dnsName)
+		}
+		return TargetInfos{
+			ip:   peer.String(),
+			name: dnsName[0],
+		}, false, false, nil
 	default:
-		return "", false, false, fmt.Errorf("wrong case")
+		return TargetInfos{}, false, false, fmt.Errorf("wrong case")
 	}
 }
--- shared/scenarios/scenarios/traceroute_test.go
+++ shared/scenarios/scenarios/traceroute_test.go
@@ -17,9 +17,6 @@ var testCasesTR = []struct {
 
 func TestTraceroute(t *testing.T) {
 	fp := NewTraceroute(testCasesTR[0].targetAddress)
-	// res, _ := fp.Run()
-	// log.Println("--> res : ", res)
-
 	res, err := fp.Run()
 	if err != nil {
 		fmt.Println("--> err : ", err.Error())
